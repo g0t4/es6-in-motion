d44d8c0d845859e0a77047a6095365a318425717
Wes Higbee <wes.mcclure@gmail.com>
2016-08-15 23:04:23 -0400

    Complex expressions in template literals

--- strings/01-interpolation/starting.js
+++ strings/01-interpolation/starting.js
@@ -1,7 +1,10 @@
 for(let number = 1; number < 5; number++) {
 
   let button = document.createElement("button");
-  let size = (number * 100) + "%";
+
+  let size = `${number * 100}%`;
+  // (number * 100) + "%";
+
   //let buttonText = "#" + number + " - " + size;
 
   let buttonText = `#${number} - ${size}`;
