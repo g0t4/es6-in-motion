4597265d89dc7ede0eb4eaa2c41df52a4c537a4a
Wes Higbee <wes.mcclure@gmail.com>
2016-08-15 22:36:37 -0400

    Starting point for string interpolation

---
+++ strings/01-interpolation/starting.js
@@ -0,0 +1,14 @@
+for(let number = 1; number < 5; number++) {
+
+  let button = document.createElement("button");
+  let size = (number * 100) + "%";
+  let buttonText = "#" + number + " - " + size;
+  button.innerText = buttonText;
+  document.body.appendChild(button);
+
+  button.addEventListener("click", function() {
+    console.log("You picked " + buttonText);
+    document.body.style.fontSize = size;
+  })
+
+}
