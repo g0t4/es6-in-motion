a09d3b8a4c82d3573f63c49237c85cb652e60a32
Wes Higbee <wes.mcclure@gmail.com>
2016-08-16 12:56:39 -0400

    Starting point for multiline

---
+++ strings/02-multiline/multiline.js
@@ -0,0 +1,28 @@
+let html = "" +
+  "<h2>" +
+  "  Application Settings" +
+  "</h2>" +
+  "<h3>" +
+  "  Please pick a font size:" +
+  "<h3>" +
+  "<ul>";
+
+for (let number = 1; number < 5; number++) {
+
+  let size = `${number * 100}%`;
+
+  html += "" +
+    "<button onclick=\"setSize('" + size + "');\">" +
+    "#" + number + " - " + size +
+    "</button>";
+
+}
+
+html += "</ul>";
+
+document.body.insertAdjacentHTML("beforeend", html);
+
+function setSize(size) {
+  console.log(`You picked ${size}`);
+  document.body.style.fontSize = size;
+}
