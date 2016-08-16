f57925b680b5ddca71b76ef14dd07a3af722a2b3
Wes Higbee <wes.mcclure@gmail.com>
2016-08-15 23:04:13 -0400

    Template literal introduction

--- strings/01-interpolation/starting.js
+++ strings/01-interpolation/starting.js
@@ -2,12 +2,15 @@ for(let number = 1; number < 5; number++) {
 
   let button = document.createElement("button");
   let size = (number * 100) + "%";
-  let buttonText = "#" + number + " - " + size;
+  //let buttonText = "#" + number + " - " + size;
+
+  let buttonText = `#${number} - ${size}`;
+
   button.innerText = buttonText;
   document.body.appendChild(button);
 
   button.addEventListener("click", function() {
-    console.log("You picked " + buttonText);
+    console.log(`You picked ${buttonText}`);
     document.body.style.fontSize = size;
   })
 
