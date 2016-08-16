3ba65db7b4b194e7b933693dc43af965256756cf
Wes Higbee <wes.mcclure@gmail.com>
2016-08-16 12:42:09 -0400

    Clean up comments to be consistent post demo

--- strings/01-interpolation/starting.js
+++ strings/01-interpolation/starting.js
@@ -3,11 +3,10 @@ for(let number = 1; number < 5; number++) {
   let button = document.createElement("button");
 
   let size = `${number * 100}%`;
-  // (number * 100) + "%";
-
-  //let buttonText = "#" + number + " - " + size;
+  // let size = (number * 100) + "%";
 
   let buttonText = `#${number} - ${size}`;
+  // let buttonText = "#" + number + " - " + size;
 
   button.innerText = buttonText;
   document.body.appendChild(button);
