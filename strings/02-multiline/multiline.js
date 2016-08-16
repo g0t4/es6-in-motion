264631efd28469413a0bafac93a755e92529e8eb
Wes Higbee <wes.mcclure@gmail.com>
2016-08-16 13:45:51 -0400

    Convert to multiline template literal

--- strings/02-multiline/multiline.js
+++ strings/02-multiline/multiline.js
@@ -1,20 +1,21 @@
-let html = "" +
-  "<h2>" +
-  "  Application Settings" +
-  "</h2>" +
-  "<h3>" +
-  "  Please pick a font size:" +
-  "<h3>" +
-  "<ul>";
+let html = `
+<h2>  
+  Application Settings
+</h2>
+
+<h3>
+  Please pick a font size:
+<h3>
+<ul>`;
 
 for (let number = 1; number < 5; number++) {
 
   let size = `${number * 100}%`;
 
-  html += "" +
-    "<button onclick=\"setSize('" + size + "');\">" +
-    "#" + number + " - " + size +
-    "</button>";
+  html += `
+<button onclick="setSize('${size}');">
+  #${number} - ${size}
+</button>`;
 
 }
 
