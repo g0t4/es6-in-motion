f8277c1ec138df4a143e3f983e09bc4e7e8c02c9
Wes Higbee <wes.mcclure@gmail.com>
2016-08-17 17:33:39 -0400

    Object literal enhancements - ending

--- destructuring/01-literals/literals.js
+++ destructuring/01-literals/literals.js
@@ -9,3 +9,23 @@ person.address = new Object();
 person.address.street = "100 Fox Lane";
 person.address.city = "New York";
 person.address.state = "NY";
+
+const pet = "cat";
+
+const person2 = {
+  first,
+  last,
+  favoriteColor: "blue",
+  address: {
+    street: "100 Fox Lane",
+    city: "New York",
+    state: "NY"
+  },
+  fullName() {
+    return `${this.first} ${this.last}`;
+  },
+  [pet]: {
+    name: "fluffly"
+  }
+};
+console.log(person2);
