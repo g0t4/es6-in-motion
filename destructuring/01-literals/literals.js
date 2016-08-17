71f975e8816a2d8ded188543f3818c755b979ad0
Wes Higbee <wes.mcclure@gmail.com>
2016-08-17 17:07:35 -0400

    Starting point for object literal assignment

---
+++ destructuring/01-literals/literals.js
@@ -0,0 +1,11 @@
+const first = "Wes";
+const last = "Higbee";
+
+const person = new Object();
+person.first = first;
+person.last = last;
+person.favoriteColor = "blue";
+person.address = new Object();
+person.address.street = "100 Fox Lane";
+person.address.city = "New York";
+person.address.state = "NY";
