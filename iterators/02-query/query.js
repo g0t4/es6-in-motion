549b3d5be4ef3265f925d45a149e98da0df00480
Wes Higbee <wes.mcclure@gmail.com>
2016-08-19 17:14:27 -0400

    Custom query iteration interface

--- iterators/02-query/query.js
+++ iterators/02-query/query.js
@@ -3,11 +3,25 @@ function PersonQuery() {
   const pax = {first: "Pax", last: "Higbee"};
   const jonathan = {first: "Jonathan", last: "Higbee"};
 
+  let theNextRecord = wes;
   this.nextRecord = function () {
-
+    switch (theNextRecord) {
+      case wes:
+        theNextRecord = pax;
+        return wes;
+      case pax:
+        theNextRecord = jonathan;
+        return pax;
+      case jonathan:
+        theNextRecord = undefined;
+        return jonathan;
+    }
+    return theNextRecord;
   }
 }
 
 const query = new PersonQuery();
+console.log(query.nextRecord());
+console.log(query.nextRecord());
 console.log(query.nextRecord());
 console.log(query.nextRecord());
