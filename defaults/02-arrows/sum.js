31fbd9159184350792caca8ffe85aa034ed1005d
Wes Higbee <wes.mcclure@gmail.com>
2016-08-19 12:28:26 -0400

    02 starting point

---
+++ defaults/02-arrows/sum.js
@@ -0,0 +1,22 @@
+const numbers = [1, 3, 4, 6, 9];
+const numbers1 = undefined;
+const numbers2 = [1, 3, undefined, 4, 6, 9];
+
+const prices = [1.10, 2.32, 3.68, 4.90];
+const taxRate = 0.07;
+
+const orders = [
+  {desc: "Pencils", price: 1.50, quantity: 2},
+  {desc: "Notepads", price: 6.00, quantity: 3},
+  {desc: "Paperclips", price: 4.30}
+];
+
+function sum(numbers = []) {
+  let sum = 0;
+  for (const [,number = 0] of numbers.entries()) {
+    sum += number;
+  }
+  return sum;
+}
+
+console.log(sum(numbers));
