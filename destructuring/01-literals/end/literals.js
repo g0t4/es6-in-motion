d71740b55816ed2d81fa892a5b6afa42e3ddacac
Wes Higbee <wes.mcclure@gmail.com>
2016-08-18 20:18:21 -0400

    Add ending point for 01

---
+++ destructuring/01-literals/end/literals.js
@@ -0,0 +1,45 @@
+const first = "Wes";
+const last = "Higbee";
+
+const person = new Object();
+person.first = first;
+person.last = last;
+person.favoriteColor = "blue";
+person.address = new Object();
+person.address.street = "100 Fox Lane";
+person.address.city = "New York";
+person.address.state = "NY";
+
+const pet = "cat";
+
+const person2 = {
+  first,
+  last,
+  favoriteColor: "blue",
+  address: {
+    street: "100 Fox Lane",
+    city: "New York",
+    state: "NY"
+  },
+  fullName() {
+    //const {first, last} = this;
+    const first = this.first;
+    const last = this.last;
+    return `${first} ${last}`;
+  },
+  [pet]: {
+    name: "fluffly"
+  }
+};
+console.log(person2);
+
+
+const numbers = new Array();
+numbers[0] = 1;
+numbers[1] = 2;
+numbers[2] = 3;
+
+const numbers2 = [1, 2, 3];
+
+
+
