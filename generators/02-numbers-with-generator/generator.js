507755b0090413b7faedb3b3ed38331b7fe02049
Wes Higbee <wes.mcclure@gmail.com>
2016-08-19 20:20:38 -0400

    02 starting point for working with generators

---
+++ generators/02-numbers-with-generator/generator.js
@@ -0,0 +1,23 @@
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+function positiveNumbersUpTo(stop) {
+  const iterable = {
+    [Symbol.iterator](){
+      // return iterator
+      let current = 1;
+      return {
+        next(){
+          if (stop < current) {
+            return {done: true};
+          }
+          return {done: false, value: current++};
+        }
+      }
+    }
+  };
+  return iterable;
+}
+
+const [...numbers] = positiveNumbersUpTo(9);
+console.log(numbers);
