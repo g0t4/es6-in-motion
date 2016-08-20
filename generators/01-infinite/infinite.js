0f85bdaf2027af45f07e181cf5ad69995b8067c2
Wes Higbee <wes.mcclure@gmail.com>
2016-08-19 20:19:26 -0400

    Stopping poing for iterator impl of positive numbers

--- generators/01-infinite/infinite.js
+++ generators/01-infinite/infinite.js
@@ -5,11 +5,20 @@ function positiveNumbersUpTo(stop) {
   const iterable = {
     [Symbol.iterator](){
       // return iterator
+      let current = 1;
+      return {
+        next(){
+          if (stop < current) {
+            return {done: true};
+          }
+          return {done: false, value: current++};
+        }
+      }
     }
   };
   return iterable;
 }
 
 //const numbers = [...positiveNumbersUpTo(9)];
-const [...numbers] = positiveNumbersUpTo(9);
+const [...numbers] = positiveNumbersUpTo(3);
 console.log(numbers);
