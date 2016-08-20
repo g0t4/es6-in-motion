4ca7bf10d0f30a572d78a47bd63f877c57578f61
Wes Higbee <wes.mcclure@gmail.com>
2016-08-20 12:21:24 -0400

    Reset 02 starting point

--- generators/02-numbers-with-generator/generator.js
+++ generators/02-numbers-with-generator/generator.js
@@ -1,28 +1,23 @@
 // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-/*
 
-function* positiveNumbersUpTo(stop) {
-  let current = 1;
-  while (current <= stop)
-    yield current++;
+
+function positiveNumbersUpTo(stop) {
+  const iterable = {
+    [Symbol.iterator](){
+      // return iterator
+      let current = 1;
+      return {
+        next(){
+          if (stop < current) {
+            return {done: true};
+          }
+          return {done: false, value: current++};
+        }
+      }
+    }
+  };
+  return iterable;
 }
 
 const [...numbers] = positiveNumbersUpTo(9);
-console.log(numbers);*/
-
-function* oneToThree() {
-  yield 1;
-  yield 2;
-  yield 3;
-}
-
-const generatorObject = oneToThree();
-console.log(generatorObject);
-const iterator = generatorObject[Symbol.iterator]();
-console.log(generatorObject === iterator);
-
-console.log(generatorObject.next());
-console.log(generatorObject.next());
-console.log(generatorObject.next());
-console.log(generatorObject.next());
-
+console.log(numbers);
