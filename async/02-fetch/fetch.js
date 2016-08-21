e321f3bfcb9f758223bad32d1ab3db6dabd40c19
Wes Higbee <wes.mcclure@gmail.com>
2016-08-21 12:33:58 -0400

    ch10c20 Understanding Error Fall Through

--- async/02-fetch/fetch.js
+++ async/02-fetch/fetch.js
@@ -1,6 +1,7 @@
 const museumsDatasetUrl = "asdf://../../datasets/museums.json";
 
 fetch(museumsDatasetUrl)
+  .catch(e => console.log("tripped"))
   .then(response => response.json())
   .then(museums => console.log(museums));
 //  .catch(error => console.log("oh no", error));
