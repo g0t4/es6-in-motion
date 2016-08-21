68cd2bd305c6562514baa701c28fcf046936886b
Wes Higbee <wes.mcclure@gmail.com>
2016-08-21 17:52:46 -0400

    Bring over ending point of delay with promises, so we can apply generator control flow to it

---
+++ async/05-control-flow/delay.js
@@ -0,0 +1,56 @@
+const messages = [`
+function getCost(supply) {
+  return supply.price * supply.quantity;
+}
+`, `
+function (supply) {
+  return supply.price * supply.quantity;
+}
+`, `
+(supply) {
+  return supply.price * supply.quantity;
+}
+`, ` 
+(supply) => {
+  return supply.price * supply.quantity;
+}
+`, `
+(supply) => supply.price * supply.quantity;
+`];
+
+const code = document.getElementById("code");
+
+code.innerText = messages[0];
+
+setTimeout(function () {
+  code.innerText = messages[1];
+  setTimeout(function () {
+    code.innerText = messages[2];
+    setTimeout(function () {
+      code.innerText = messages[3];
+      setTimeout(function () {
+        code.innerText = messages[4];
+      }, 500);
+    }, 500);
+  }, 500);
+}, 500);
+
+delay(500)
+  .then(() => code.innerText = messages[1])
+  .then(() => delay(500))
+  .then(() => code.innerText = messages[2])
+  .then(() => delay(500))
+  .then(() => code.innerText = messages[3])
+  .then(() => delay(1500))
+  .then(() => code.innerText = messages[4]);
+
+function delay(duration) {
+  function executor(resolve, reject) {
+    setTimeout(function () {
+      // trigger success
+      resolve();
+    }, duration);
+  }
+
+  return new Promise(executor);
+}
