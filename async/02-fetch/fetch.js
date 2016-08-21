ccfae5a53b27e2d46519b7dfacda215d89dd9693
Wes Higbee <wes.mcclure@gmail.com>
2016-08-21 11:28:48 -0400

    ch10b20 Introducing Promises and Fetch

--- async/02-fetch/fetch.js
+++ async/02-fetch/fetch.js
@@ -1,1 +1,10 @@
 const museumsDatasetUrl = "../../datasets/museums.json";
+
+const requestPromise = fetch(museumsDatasetUrl);
+requestPromise.then(function (response) {
+  const parsePromise = response.json();
+  parsePromise.then(function (museums) {
+    console.log(museums);
+  });
+  console.log(response);
+});
