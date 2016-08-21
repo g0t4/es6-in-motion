5698289c34f86d2433b60cef6a7567c227ee14f3
Wes Higbee <wes.mcclure@gmail.com>
2016-08-21 12:19:55 -0400

    error handler with then method

--- async/02-fetch/fetch.js
+++ async/02-fetch/fetch.js
@@ -2,5 +2,4 @@ const museumsDatasetUrl = "../../atasets/museums.json";
 
 fetch(museumsDatasetUrl)
   .then(response => response.json())
-  .then(museums => console.log(museums))
-  .catch(error => console.log("oh no", error));
+  .then(museums => console.log(museums), error => console.log("oh no", error));
