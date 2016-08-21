b5b8441125f959e5ca674d691957b84a8ce0d287
Wes Higbee <wes.mcclure@gmail.com>
2016-08-21 17:34:08 -0400

    Change to fetch starting point

--- async/05-control-flow/generators.js
+++ async/05-control-flow/generators.js
@@ -1,55 +1,30 @@
-const messages = [`
-function getCost(supply) {
-  return supply.price * supply.quantity;
-}
-`, `
-function (supply) {
-  return supply.price * supply.quantity;
-}
-`, `
-(supply) {
-  return supply.price * supply.quantity;
-}
-`, ` 
-(supply) => {
-  return supply.price * supply.quantity;
-}
-`, `
-(supply) => supply.price * supply.quantity;
-`];
+const museumsDatasetUrl = "../../datasets/museums.json";
 
-const code = document.getElementById("code");
+fetch(museumsDatasetUrl)
+  .then(response => response.json())
+  .then(museums => console.log(museums))
+  .catch(error => console.log("oh no", error));
 
-code.innerText = messages[0];
-/*
 
-delay(500)
-  .then(() => code.innerText = messages[1])
-  .then(() => delay(500))
-  .then(() => code.innerText = messages[2])
-  .then(() => delay(500))
-  .then(() => code.innerText = messages[3])
-  .then(() => delay(1500))
-  .then(() => code.innerText = messages[4]);
-*/
 
-function* program() {
 
-}
 
-async(program);
 
-function delay(duration) {
-  function executor(resolve, reject) {
-    setTimeout(function () {
-      // trigger success
-      resolve();
-    }, duration);
-  }
 
-  return new Promise(executor);
-}
 
+
+
+
+
+
+
+
+
+
+
+
+  
+  
 // copied from: http://es6-features.org/#GeneratorControlFlow
 // MIT License
 // generic asynchronous control-flow driver
