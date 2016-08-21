6b4e97862708f83b214dff07ed104d2a85a6cf2a
Wes Higbee <wes.mcclure@gmail.com>
2016-08-21 12:46:19 -0400

    Catching thrown errors too

--- async/02-fetch/fetch.js
+++ async/02-fetch/fetch.js
@@ -1,10 +1,13 @@
 const museumsDatasetUrl = "../../atasets/museums.json";
 
 fetch(museumsDatasetUrl)
-  .then(response => response.json())
+  .then(function (response) {
+    throw "whatever";
+    return response.json();
+  })
   .catch(function (e) {
     console.log("tripped");
-    return [1,2,3];
+    return [1, 2, 3];
   })
   .then(museums => console.log(museums));
 //  .catch(error => console.log("oh no", error));
