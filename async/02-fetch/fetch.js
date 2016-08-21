6c2e9fe15f98d0496db1ab66f57f614bf27caa82
Wes Higbee <wes.mcclure@gmail.com>
2016-08-21 12:10:41 -0400

    ch10b40 Concise Async With Arrow Functions

--- async/02-fetch/fetch.js
+++ async/02-fetch/fetch.js
@@ -1,9 +1,5 @@
 const museumsDatasetUrl = "../../datasets/museums.json";
 
 fetch(museumsDatasetUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (museums) {
-    console.log(museums);
-  });
+  .then(response => response.json())
+  .then(museums => console.log(museums));
