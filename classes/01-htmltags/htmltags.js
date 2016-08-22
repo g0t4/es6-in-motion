f5ad23c18054bc8d0689a5e0abb2845c5cf93616
Wes Higbee <wes.mcclure@gmail.com>
2016-08-22 11:04:52 -0400

    ch12c10 Instance Methods

--- classes/01-htmltags/htmltags.js
+++ classes/01-htmltags/htmltags.js
@@ -25,8 +25,16 @@ const container = new Div()
   .addChild(new Div()
     .addChild(availability));
 
+const input = new Input().setType("text");
+const input2 = new Input().setType("checkbox");
+const input3 = new Input().setType("radio");
+
 const content = document.getElementById("content");
-content.innerHTML = container.render();
+content.innerHTML = new Div()
+  .addChild(input)
+  .addChild(input2)
+  .addChild(input3)
+  .render();
 
 function Input() {
 
