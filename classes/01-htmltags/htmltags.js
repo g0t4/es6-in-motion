b82f8d2bb339ecb867b712e5fa2c379d0968ead4
Wes Higbee <wes.mcclure@gmail.com>
2016-08-22 14:56:47 -0400

    Extract list item constructur functions to own file

--- classes/01-htmltags/htmltags.js
+++ classes/01-htmltags/htmltags.js
@@ -74,27 +74,3 @@ function UnorderedList() {
   };
 }
 
-function TextListItem(text) {
-
-  ListItem.call(this);
-
-  this.addChild(new TextSpan(text));
-
-}
-
-function ListItem() {
-  this.children = [];
-
-  this.addChild = function (element) {
-    this.children.push(element);
-    return this;
-  };
-
-  this.render = function () {
-    const childrenHtml = this.children
-      .map(c => c.render())
-      .join("");
-
-    return `<li>${childrenHtml}</li>`
-  };
-}
